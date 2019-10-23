ab4f62fb7ea077b1704df73c49938e872dd4f5f9
yeltnar <andcbrant@gmail.com>
2019-10-23 01:17:45 -0500

    Update cloud_scripts_browser_connector.js

--- cloud_scripts_browser_connector.js
+++ cloud_scripts_browser_connector.js
@@ -4,22 +4,29 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://70.120.122.87:3333/cloud_scripts/*
+// @match        https://70.120.122.87:3333/cloud_scripts/run
+// @match        https://www.fedex.com/apps/fedextracking/
+// @match        https://*
+// @match        http://*
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
-//will currently run all the files at once
-const obj_files_to_run = {
-    "https://70.120.122.87:3333/cloud_scripts/run":[
-        "https://raw.githubusercontent.com/yeltnar/browser_cloud_scripts/master/fedex_tracking",
-        //"https://70.120.122.87:3333/cloud_scripts/t.txt"
-    ]
-};
 
 (function() {
     'use strict';
 
     console.log("Cloud Scripts Browser Connector");
+
+    //will currently run all the files at once
+    const obj_files_to_run = {
+        "https://70.120.122.87:3333/cloud_scripts/run":[
+            "https://raw.githubusercontent.com/yeltnar/browser_cloud_scripts/master/fedex_tracking",
+            //"https://70.120.122.87:3333/cloud_scripts/t.txt"
+        ],
+        "https://www.fedex.com/apps/fedextracking/":[
+            "https://raw.githubusercontent.com/yeltnar/browser_cloud_scripts/master/fedex_tracking"
+        ]
+    };
 
     const files_to_run = getWindowsFilesToRun(location.href) || [];
 
@@ -38,10 +45,10 @@ const obj_files_to_run = {
         eval(file_content);
     });
 
-    // Your code here...
+    function getWindowsFilesToRun(){
+        return obj_files_to_run[location.href] || false;
+    }
+
 })();
 
-function getWindowsFilesToRun(){
-    return obj_files_to_run[location.href] || false;
-}
 
