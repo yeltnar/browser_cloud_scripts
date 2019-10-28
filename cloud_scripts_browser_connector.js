a0bef67477f16f30d4916f7be0461fa20f891875
yeltnar <andcbrant@gmail.com>
2019-10-28 15:45:49 -0500

    Update cloud_scripts_browser_connector.js

--- cloud_scripts_browser_connector.js
+++ cloud_scripts_browser_connector.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cloud Scripts Browser Connector
 // @namespace    http://tampermonkey.net/
-// @version      0.3.4
+// @version      0.3.5
 // @description  try to take over the world!
 // @author       You
 // @match        https://70.120.122.87:3333/cloud_scripts/run
@@ -9,8 +9,8 @@
 // @match        https://*
 // @match        http://*
 // @match        http://*/*
-// @match        https://*/*
 // @match        chrome-extension://*/*
+// @match        https://*/*
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
@@ -23,17 +23,42 @@
     let files_to_run = [];
 
     const source_list = [
-        //"https://raw.githubusercontent.com/yeltnar/browser_cloud_scripts/master/url_file_map.json",
-        "https://droodle_b.keybase.pub/bad_idea/files_to_run.json"
+        "https://raw.githubusercontent.com/yeltnar/browser_cloud_scripts/master/url_file_map.json",
+        // "https://droodle_b.keybase.pub/bad_idea/files_to_run.json" // won't work with cors
     ];
+
+    fetch(source_list[1], {mode: '*cors'}).then((resp)=>{
+        return resp.json();
+    }).then((json)=>{
+        console.log(json);
+    });
+
+    return;
+
+    console.log({source_list});
 
     await Promise.all(source_list.map(( cur, i, arr )=>{
 
         return new Promise(async(resolve, reject)=>{
 
-            const resp = await fetch(cur).then((r)=>{
-                return r.json();
+            console.log({cur_source:cur});
+
+            console.log(`look here 1`);
+
+            const resp = await (fetch(cur, {mode: 'cors'}).then(async(r)=>{
+                const json = await r.json();
+                return json;
+            })).catch((e)=>{
+                console.error(e);
+                return {"error":"error",e};
             });
+
+            if(resp.error==="error"){
+                alert(JSON.stringify(resp));
+                return;
+            }
+
+            alert(`look here ${resp}`);
 
             resp.forEach((cur)=>{
 
@@ -41,6 +66,9 @@
 
                 if( regex.test(window.location.href) ){
                     files_to_run = files_to_run.concat(cur.scripts);
+                    console.log(`added ${JSON.stringify(cur.scripts)}`);
+                }else{
+                    console.log(`not adding ${regex}`);
                 }
             });
 
