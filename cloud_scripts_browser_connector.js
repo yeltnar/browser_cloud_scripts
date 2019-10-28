262fa0d80657e43ec10878a9ed8c1bb2158656e1
yeltnar <andcbrant@gmail.com>
2019-10-28 10:38:41 -0500

    Update cloud_scripts_browser_connector.js

--- cloud_scripts_browser_connector.js
+++ cloud_scripts_browser_connector.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cloud Scripts Browser Connector
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://70.120.122.87:3333/cloud_scripts/run
