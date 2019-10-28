86068eb676fc786f79c423a9f34e288f0e6c2dd6
yeltnar <andcbrant@gmail.com>
2019-10-28 10:49:57 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Cloud Scripts Browser Connector
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to take over the world!
+// @author       You
+// @match        https://70.120.122.87:3333/cloud_scripts/run
+// @match        https://www.fedex.com/apps/fedextracking/
+// @match        https://*
+// @match        http://*
+// @grant        GM.xmlHttpRequest
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    
+    alert("hello");
+
+})();
+
+
